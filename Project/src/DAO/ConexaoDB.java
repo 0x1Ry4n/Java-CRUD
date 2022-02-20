182890af49c4839522e59ebe0b6405758681d423
Ryan <58119359+0x1Ry4n@users.noreply.github.com>
2022-02-20 15:21:19 -0300

    Update ConexaoDB.java

--- Project/src/DAO/ConexaoDB.java
+++ Project/src/DAO/ConexaoDB.java
@@ -9,7 +9,7 @@ public class ConexaoDB {
     public Connection conectaBD(){ //método público
           Connection conn = null;  //Objeto de conexão
           try{ //tente
-            String url = "jdbc:mysql://localhost:3306/bd_pizzaria?user=root&password="; //url do localhost que se conecta ao banco 
+            String url = "jdbc:mysql://localhost:3306/database?user=&password="; //url do localhost que se conecta ao banco 
             conn = DriverManager.getConnection(url); //estabelecer a conexão entre o localhost e o java 
           }catch(SQLException erro){ //exceto
             JOptionPane.showMessageDialog(null, erro.getMessage()); //mostra a mensagem de erro se o Exception for chamado
